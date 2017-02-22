1d17c532238fe314d77165ba27faaeb514752d40
Kostiuk Inna <innakoss@Kostiuk-no-MacBook-Pro.local>
2017-02-22 12:37:41 +0900

    add project

---
+++ static/js/slideanim.js
@@ -0,0 +1,16 @@
+/******************
+* slide animation *
+******************/
+
+$(document).ready(function(){  
+	$(window).scroll(function() {
+		$(".slideanim").each(function(){
+			var pos = $(this).offset().top;
+
+			var winTop = $(window).scrollTop();
+			if (pos < winTop + 600) {
+				$(this).addClass("slide");
+			}
+		});
+	});
+})
